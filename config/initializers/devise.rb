cd78d76a700fe90b6a428c4949fc2b391b3d994b
Natalie Tan <natalieytan@gmail.com>
2018-05-04 12:19:47 +1000

    User model created with Devise

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -179,7 +179,13 @@ Devise.setup do |config|
   # Defines which strategy will be used to lock an account.
   # :failed_attempts = Locks an account after a number of failed attempts to sign in.
   # :none            = No lock strategy. You should handle locking by yourself.
-  # config.lock_strategy = :failed_attempts
+  config.lock_strategy = :failed_attempts
+  config.unlock_keys = [:email]
+  config.unlock_strategy = :both
+  config.maximum_attempts = 10
+  config.unlock_in = 1.hour
+  config.last_attempt_warning = true
+  config.reset_password_within = 6.hours
 
   # Defines which key will be used when locking and unlocking an account
   # config.unlock_keys = [:email]
