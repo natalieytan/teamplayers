01ff2112aa1d4ed7b16ce389244232738bc3ff98
Natalie Tan <natalieytan@gmail.com>
2018-05-08 15:03:02 +1000

    Add team name and description validation

--- app/models/team.rb
+++ app/models/team.rb
@@ -7,6 +7,8 @@ class Team < ApplicationRecord
   belongs_to :day
   geocoded_by :full_street_address 
   after_validation :geocode
+  validates :name, presence: true
+  validates :description, length: { maximum: 1000 }
 
   def full_street_address
     "#{street_number} #{route}, #{administrative_area} #{postal_code}, #{country}"
