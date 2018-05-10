1cb374cf90bea464a9b89b649645a4d38b076f8b
Natalie Tan <natalieytan@gmail.com>
2018-05-11 00:19:22 +1000

    Greatly Improved Search CSS

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,19 +1,25 @@
 class SearchController < ApplicationController
   def teams
+    @teams = Team.none
     if params[:teamsearch]
       search_params = params.require(:teamsearch).permit(:location, :distance, :sport_id, skill_id:[], gender_id: [])
-      @location = search_params[:location]
-      distance = search_params[:distance] || 5
-      search_criteria = search_params.slice(:sport_id, :skill_id, :gender_id)
-      @teams = @location.present? ? Team.includes(:gender, :sport, :skill).near(location, distance, units: :km).where(search_criteria) : Team.includes(:gender, :sport, :skill).where(search_criteria)
-      @teams.present? ? flash.now[:notice] = "Showing teams which match your search." : flash.now[:alert] = "Could not find any team that matched your search requests."
+      if params[:teamsearch][:sport_id].empty?
+        flash.now[:alert] = "Please enter a sport you are searching for."
+        render :teams
+      else
+        location = search_params[:location]
+        distance = search_params[:distance] || 5
+        @coords = Geocoder.coordinates(location)
+        search_criteria = search_params.slice(:sport_id, :skill_id, :gender_id)
+        @teams = @location.present? ? Team.includes(:gender, :sport, :skill).near(location, distance).where(search_criteria) : Team.includes(:gender, :sport, :skill).where(search_criteria)
+        @teams.present? ? flash.now[:notice] = "Showing teams which match your search." : flash.now[:alert] = "Could not find any team that matched your search requests."
+      end
     else
-      @teams = Team.none
     end
   end
 
   def players
-    @players = User.none
+    @players = Profile.none
     if params[:playersearch]
       sport = params[:interests][:sport_id]
       gender = params[:profile][:gender]
@@ -21,9 +27,9 @@ class SearchController < ApplicationController
         flash.now[:alert] = "Please specify a sport and gender to find a player."
       else
         skill = params[:interests][:skill] || [1, 2, 3, 4, 5]
-        location = params[:playersearch][:location]
+        @location = params[:playersearch][:location]
         distance = params[:playersearch][:distance] || 5 
-        @players = @location.present? ? User.joins(:interests, :profile).near(location, distance, units: :km).where(interests: {sport_id: sport, skill: skill}, profiles: {gender: gender}) : User.joins(:interests, :profile).where(interests: {sport_id: sport, skill: skill}, profiles: {gender: gender})
+        @players = @location.present? ? Profile.joins(user: :interests).near(@location, distance).where(interests: {sport_id: sport, skill: skill}, gender: gender) : Profile.joins(user: :interests).where(interests: {sport_id: sport, skill: skill}, gender: gender)
         flash.now[:notice] = "Showing players who match your search."
       end
     else
--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,5 +1,6 @@
 Geocoder.configure(
   :use_https => true,
+  :units => :km,
   :google=> {
   :api_key => ENV['GEOCODER_API']
   }  
