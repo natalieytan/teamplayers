663efb89327cf83bb77b5da9526d9398401e7984
Natalie Tan <natalieytan@gmail.com>
2018-05-11 05:46:59 +1000

    Bug fix

--- app/models/game_player.rb
+++ app/models/game_player.rb
@@ -2,5 +2,5 @@ class GamePlayer < ApplicationRecord
   enum attendance_status: { awaiting_response: 1, attending: 2, not_attending: 3}
   belongs_to :game
   belongs_to :user
-  #validates_uniqueness_of :user_id, {:scope => :game_id, message: "can only attend each game once."}
+  validates_uniqueness_of :user_id, {:scope => :game_id, message: "can only attend each game once."}
 end
