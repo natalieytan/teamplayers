ba75b4716fd4f6e225655951d0c60741b08b3ba4
Natalie Tan <natalieytan@gmail.com>
2018-05-11 04:49:29 +1000

    Teams seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -88,6 +88,30 @@ require 'csv'
 # end
 
 # seed some interests
+# prng = Random.new
+# users = User.last(99)
+# users.each { |user| user.interests.create!(user: user, sport_id: prng.rand(1..9), skill: prng.rand(1..5)) }
+
+
+# seed some teams
 prng = Random.new
-users = User.last(99)
-users.each { |user| user.interests.create!(user: user, sport_id: prng.rand(1..9), skill: prng.rand(1..5)) }
+50.times do |n|
+  Team.create!({
+    owner_id: prng.rand(2..99),
+    sport_id: prng.rand(1..9),
+    skill_id: prng.rand(1..5),
+    gender_id: prng.rand(1..3),
+    day_id: prng.rand(1..7),
+    name: Faker::Team.name,
+    description: Faker::Seinfeld.quote,
+    street_number: Faker::Address.building_number,
+    route: Faker::Address.street_name,
+    locality: Faker::Address.city,
+    administrative_area: Faker::Address.state,
+    postal_code: Faker::Address.postcode,
+    country: Faker::Address.country,
+    latitude: Faker::Address.latitude,
+    longitude: Faker::Address.longitude,
+
+  })
+end
